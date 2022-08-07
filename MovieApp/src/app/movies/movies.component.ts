3ea884006328129718125c1fe18bec7da78381d4
NurettinKaynar <nurettinkaynar568@gmail.com>
2022-08-07 12:34:48 +0300

    populer filmler eklendi

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -10,12 +10,14 @@ import { MovieRepository } from 'src/models/movie.repository';
 export class MoviesComponent implements OnInit {
   // movies = ['Film 1', 'Film 2', 'Film 3'];
   movies: Movie[];
-
+  title: string = 'Film Listesi';
   movieRepository: MovieRepository;
+  popularMovies: Movie[];
 
   constructor() {
     this.movieRepository = new MovieRepository();
     this.movies = this.movieRepository.getMovies();
+    this.popularMovies = this.movieRepository.getPopularMovies();
   }
 
   ngOnInit(): void {}
--- MovieApp/src/models/movie.repository.ts
+++ MovieApp/src/models/movie.repository.ts
@@ -7,36 +7,51 @@ export class MovieRepository {
     this.movies = [
       {
         id: 1,
-        title: 'film 1',
+        title: 'Shazam 6 Güç',
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, in!',
         imageUrl: '1.jpeg',
+        isPopular: true,
       },
       {
         id: 2,
-        title: 'film 2',
+        title: 'Amazing Grace',
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, in!',
         imageUrl: '2.jpeg',
+        isPopular: false,
       },
       {
         id: 3,
-        title: 'film 3',
+        title: 'High Life',
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, in!',
         imageUrl: '3.jpeg',
+        isPopular: false,
       },
       {
         id: 4,
-        title: 'film 4',
+        title: 'Billboard',
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, in!',
         imageUrl: '4.jpeg',
+        isPopular: true,
+      },
+      {
+        id: 5,
+        title: 'Storm Boy',
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, in!',
+        imageUrl: '5.jpeg',
+        isPopular: true,
       },
     ];
   }
   getMovies(): Movie[] {
     return this.movies;
+  }
+  getPopularMovies(): Movie[] {
+    return this.movies.filter((i) => i.isPopular);
   }
   getMovieById(id: number): Movie {
     return this.movies.find((i) => i.id == id);
